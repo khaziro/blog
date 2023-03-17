cf334930abd27eb0d83c850064f427b33b022edf
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-17 12:13:18 +0200

    Added a loading message for when the data is not finished fetching

--- src/home.jsx
+++ src/home.jsx
@@ -3,6 +3,7 @@ import BlogList from "./blog-list.jsx";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -10,16 +11,18 @@ const Home = () => {
         return response.json()
       })
       .then(data => {
-        console.log(data);
         setBlogs(data);
+        setIsPending(false);
       })
   }, [])
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs}
                           title="All blogs"
-                          />}
+      />}
+
     </div>
   )
 }
