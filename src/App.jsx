a1a8ce9ce1834ecbc2b0f3afd78f096eaff9ce6b
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-17 23:22:29 +0200

    Set up our React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import Navbar from "./navbar.jsx";
 import Home from './home'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Create from "./create.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Routes>
+            <Route exact
+                   path='/'
+                   element={<Home/>}
+            >
+            </Route>
+            <Route path='/create'
+                   element={<Create/>}
+            >
+            </Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
