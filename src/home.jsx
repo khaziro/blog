0e075ad1457fadebc493cfe4994f1ecf05ce7e6f
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-17 11:51:57 +0200

    Removed the handleDelete function, added useEffect to fetch data and conditionally render the BlogList only if the useState has a value

--- src/home.jsx
+++ src/home.jsx
@@ -1,23 +1,25 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import BlogList from "./blog-list.jsx";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {title: 'Blog 1', body: 'Blog body bla bla bla', author: 'Mochi', id: 1,},
-    {title: 'Blog 2', body: 'Blog body bla bla bla', author: 'Mia', id: 2,},
-    {title: 'Blog 3', body: 'Blog body bla bla bla', author: 'Patrick', id: 3,}
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs)
-  }
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data);
+        setBlogs(data);
+      })
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs}
-                title="All blogs"
-                handleDelete={handleDelete}/>
+      {blogs && <BlogList blogs={blogs}
+                          title="All blogs"
+                          />}
     </div>
   )
 }
