1859719d09c1f0c086895cedccd63f701554f2ca
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-03-17 21:23:00 +0200

    Added a catch for the errors

--- src/home.jsx
+++ src/home.jsx
@@ -4,20 +4,30 @@ import BlogList from "./blog-list.jsx";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(response => {
+        if (!response.ok) {
+          throw Error('Could not fetch the data for that resource')
+        }
         return response.json()
       })
       .then(data => {
         setBlogs(data);
         setIsPending(false);
+        setError(null)
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
       })
   }, [])
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs}
                           title="All blogs"
